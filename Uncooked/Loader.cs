0c064cd34ec72b6d7b35507ee5a02faf4d643f34
Shiv Trivedi <shiv.v.trivedi@gmail.com>
2021-07-01 17:26:21 -0400

    basic injection

---
+++ Uncooked/Loader.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Uncooked
+{
+    public class Loader
+    {
+        private static GameObject _load;
+        public static void Init()
+        {
+            _load = new GameObject();
+            _load.AddComponent<Main>();
+            GameObject.DontDestroyOnLoad(_load);
+        }
+        public static void Unload()
+        {
+            _Unload();
+        }
+        private static void _Unload()
+        {
+            GameObject.Destroy(_load);
+        }
+        private GameObject _gameObject;
+    }
+}
---
+++ Uncooked/Main.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Uncooked
+{
+    class Main : MonoBehaviour
+    {
+        public void Start()
+        {
+        }
+        public void Update()
+        {
+        }
+        public void OnGUI()
+        {
+            // Here you can call IMGUI functions of Unity to build your UI for the hack :)
+            GUI.Label(new Rect(Screen.width / 2, Screen.height / 2, 150f, 50f), "GAME INJECTED"); // Should work and when injected you will see this text in the middle of the screen
+        }
+    }
+}
